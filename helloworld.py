d611cf851af29481d305a82504acad71495da38b
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2024-05-26 00:18:37 +0330

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,7 +0,0 @@
-import requests
-import base64
-link= requests.get('https://panel1.nimplus.space/api/user/account/subscribe/9e0132ca39b4cfe9dde5be32eab967c2?type=android&ip=0&flow=1')
-
-code=link.text
-##encode= base64.b64encode(b'code')
-print(code)
