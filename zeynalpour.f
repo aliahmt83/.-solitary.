169957685aa64c09124f497ee3cecf155853ff19
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-07-01 19:09:10 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,6 +1,6 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1MjgwMg==#38.18GB/100.00GB (38.2 %)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1MjgwMg==#38.91GB/100.00GB (38.9 %)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1MjgwMg==#1404/04/13 13%3A29%3A11 
-vless://d0cda383-9738-a260-f78f-87bdba2af1f4@188.213.170.76:80?encryption=none&type=grpc#TamamBaha %F0%9F%87%AE%F0%9F%87%B9 
+vless://d0cda383-9738-a260-f78f-87bdba2af1f4@89.36.208.205:80?encryption=none&type=grpc#TamamBaha %F0%9F%87%AE%F0%9F%87%B9 
 vless://d0cda383-9738-a260-f78f-87bdba2af1f4@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#GRPC %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
 vless://d0cda383-9738-a260-f78f-87bdba2af1f4@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#XHTTP %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
 vless://d0cda383-9738-a260-f78f-87bdba2af1f4@cdn2.myandroidplus2.ir:80?mode=auto&path=/&security=none&encryption=none&type=xhttp#XHTTP %F0%9F%87%AB%F0%9F%87%B7 
