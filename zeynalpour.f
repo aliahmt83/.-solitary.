d471202328c3d265bfe412e99abe4a0a10d4647b
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-07-04 12:34:52 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,8 +1,8 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1MjgwMg==#40.54GB/100.00GB (40.5 %)
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1MjgwMg==#24 This will expire min 
-vless://d0cda383-9738-a260-f78f-87bdba2af1f4@89.36.208.205:80?encryption=none&type=grpc#TamamBaha %F0%9F%87%AE%F0%9F%87%B9 
-vless://d0cda383-9738-a260-f78f-87bdba2af1f4@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#GRPC %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
-vless://d0cda383-9738-a260-f78f-87bdba2af1f4@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#XHTTP %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
-vless://d0cda383-9738-a260-f78f-87bdba2af1f4@cdn2.myandroidplus2.ir:80?mode=auto&path=/&security=none&encryption=none&type=xhttp#XHTTP %F0%9F%87%AE%F0%9F%87%B9 
-vless://d0cda383-9738-a260-f78f-87bdba2af1f4@speed2.wiselearn.ir:80?encryption=none&type=grpc#Tamambaha %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
-vless://d0cda383-9738-a260-f78f-87bdba2af1f4@speed.wiselearn.ir:80?encryption=none&type=grpc#Tamambaha %F0%9F%87%AB%F0%9F%87%AE%E2%9A%A1 
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTozWkpSMG1lNUZRQGxvY2FsaG9zdDo1NDc4MA==#0B/60.00GB (0.0 %)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTozWkpSMG1lNUZRQGxvY2FsaG9zdDo1NDc4MA==#1404/05/12 13%3A29%3A02 
+vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@89.36.208.205:80?encryption=none&type=grpc#TamamBaha %F0%9F%87%AE%F0%9F%87%B9 
+vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#GRPC %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
+vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#XHTTP %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
+vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@cdn2.myandroidplus2.ir:80?mode=auto&path=/&security=none&encryption=none&type=xhttp#XHTTP %F0%9F%87%AE%F0%9F%87%B9 
+vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@speed2.wiselearn.ir:80?encryption=none&type=grpc#Tamambaha %F0%9F%87%A9%F0%9F%87%AA%E2%9A%A1 
+vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@speed.wiselearn.ir:80?encryption=none&type=grpc#Tamambaha %F0%9F%87%AB%F0%9F%87%AE%E2%9A%A1 
