1f9ac8627358abc68b051a778db554f1a51c6369
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-07-04 18:35:16 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,8 +1,8 @@
-vless://abcd1234-5678-90ab-cdef-1234567890ab@localhost.com:443?encryption=none&type=grpc#0B/60.00GB (0.0 %)
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1NDc4MA==#1404/05/12 13:29:02
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@89.36.208.205:80?encryption=none&type=grpc#🇮🇹
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#🇩🇪
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#🇩🇪
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:80?mode=auto&path=/&security=none&encryption=none&type=xhttp#🇮🇹
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed.wiselearn.ir:80?encryption=none&type=grpc#🇫🇮
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1NDc4MA==#0B/60.00GB (0.0 درصد)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1NDc4MA==#1404/05/12 13:29:02 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@89.36.208.205:80?encryption=none&type=grpc#🇮🇹 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#🇩🇪 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#🇩🇪 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:80?mode=auto&path=/&security=none&encryption=none&type=xhttp#🇮🇹 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed.wiselearn.ir:80?encryption=none&type=grpc#🇫🇮 
