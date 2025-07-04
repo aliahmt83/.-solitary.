102e871e7c87dd060fc40c841409a993c25fef38
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-07-04 14:42:47 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,4 +1,4 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpOUDNaSlIwbWU1RlFAbG9jYWxob3N0OjU0Nzgw#0B/60.00GB (0.0 %)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpOUDNaSlIwbWU1RlFAbG9jYWxob3N0OjUwMDU1#0B/60.00GB (0.0 %)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTozWkpSMG1lNUZRQGxvY2FsaG9zdDo1NDc4MA==#1404/05/12 13:29:02
 vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@89.36.208.205:80?encryption=none&type=grpc#🇮🇹
 vless://a5527231-872c-eb6b-6bb1-825e8b3fa062@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#🇩🇪
