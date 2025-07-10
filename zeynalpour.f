d31fcf485bc7b7c291d89bf0a1cae598d772dc9c
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-07-10 16:22:14 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -3,8 +3,8 @@ ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1NDc4MA==#1404/0
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@it.wiselearn.ir:80?encryption=none&type=tcp#🇮🇹 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@89.36.208.205:80?encryption=none&type=grpc#🇮🇹 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp#🇹🇷 
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp&path=/ru#(🇷🇺)روسیه 
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#g(Tunnel) 
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#x(Tunnel) 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp&path=/ru#🇷🇺 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8082?encryption=none&type=grpc#Tunnel 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#Tunnel 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed.wiselearn.ir:80?encryption=none&type=grpc#🇫🇮 
