ee61818272a7244914f0105acbaad54b3b3dc4ef
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-07-12 17:04:57 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,11 +1,11 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1NDc4MA==#3.44GB/60.00GB (5.7 درصد)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1NDc4MA==#3.50GB/60.00GB (5.8 درصد)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyQGxvY2FsaG9zdDo1NDc4MA==#1404/05/12 13:29:02 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@it.wiselearn.ir:80?encryption=none&type=tcp#🇮🇹 
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@141.98.233.104:80?mode=auto&path=/&security=none&encryption=none&type=xhttp#🇧🇪 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@fast.vipstore.world:80?mode=auto&path=/&security=none&encryption=none&extra={"scMaxEachPostBytes": "1000000", "scMaxConcurrentPosts": 100, "scMinPostsIntervalMs": 30, "xPaddingBytes": "100-1000", "noGRPCHeader": false}&host=belgium.global.ssl.fastly.net&type=xhttp#🇧🇪 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp#🇹🇷 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp&path=/ru#🇷🇺 
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@ned.vipstore.world:80?mode=auto&path=/&security=none&encryption=none&type=xhttp#🇳🇱 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@fast.vipstore.world:80?mode=auto&path=/&security=none&encryption=none&extra={"scMaxEachPostBytes": "1000000", "scMaxConcurrentPosts": 100, "scMinPostsIntervalMs": 30, "xPaddingBytes": "100-1000", "noGRPCHeader": false}&host=holland.global.ssl.fastly.net&type=xhttp#🇳🇱 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@cdn2.myandroidplus2.ir:8080?mode=auto&path=/&security=none&encryption=none&type=xhttp#Tunnel 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
 vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@speed.wiselearn.ir:8880?encryption=none&type=grpc#🇫🇮 
-vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@65.108.242.114:80?mode=auto&security=none&encryption=none&path=/api&type=xhttp#🇫🇮 
+vless://78ca472b-7094-9ffe-5bce-47fa69157ffc@fast.vipstore.world:80?mode=auto&path=/api&security=none&encryption=none&extra={"scMaxEachPostBytes": "1000000", "scMaxConcurrentPosts": 100, "scMinPostsIntervalMs": 30, "xPaddingBytes": "100-1000", "noGRPCHeader": false}&host=finland1.global.ssl.fastly.net&type=xhttp#🇫🇮 
