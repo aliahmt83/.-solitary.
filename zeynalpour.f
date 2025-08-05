755b53935f4c69671887f896d847506434245205
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-08-05 09:24:12 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,11 +1,11 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTotUmV6YS1AbG9jYWxob3N0OjU0MTUx#52.56GB/60.00GB (87.6 درصد)
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTotUmV6YS1AbG9jYWxob3N0OjU0MTUx#1404/05/17 20:00:08 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@141.98.233.104:80?mode=auto&path=/&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇧🇪 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp#🇹🇷 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp&path=/ru#🇷🇺 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@ned.vipstore.world:80?mode=auto&path=/&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇳🇱 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#Tunnel 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@it.wiselearn.ir:80?path=/&encryption=none&type=tcp#🇮🇹 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@speed.wiselearn.ir:8880?encryption=none&type=grpc#🇫🇮 
-vless://e7716b3b-efa1-b126-fbd5-0f988bda5ee7@finland.vipstore.world:80?mode=auto&path=/api&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇫🇮 
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#0B/60.00GB (0.0 درصد)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#1404/06/13 10:20:21 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@141.98.233.104:80?mode=auto&path=/&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇧🇪 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp#🇹🇷 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp&path=/ru#🇷🇺 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@ned.vipstore.world:80?mode=auto&path=/&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇳🇱 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#Tunnel 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@it.wiselearn.ir:80?path=/&encryption=none&type=tcp#🇮🇹 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@speed.wiselearn.ir:8880?encryption=none&type=grpc#🇫🇮 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@finland.vipstore.world:80?mode=auto&path=/api&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇫🇮 
