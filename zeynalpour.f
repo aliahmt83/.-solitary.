7a288da7a469e5b2938dc743ba30e51a4e761f13
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-08-15 08:45:06 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,9 +1,9 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#6.83GB/60.00GB (11.4 درصد)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#6.90GB/60.00GB (11.5 درصد)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#1404/06/13 10:20:21 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@141.98.233.104:80?mode=auto&path=/&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇧🇪 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp&sni=tr.wiselearn.ir#🇹🇷 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@r.wiselearn.ir:8443?security=tls&alpn=h2&encryption=none&headerType=&type=tcp&path=/ru&sni=r.wiselearn.ir#🇷🇺 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@ned.vipstore.world:80?mode=auto&path=/&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇳🇱 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#🇳🇱 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:80?path=/&encryption=none&type=xhttp#🇺🇸 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#Tunnel 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@it.wiselearn.ir:80?path=/&encryption=none&type=tcp#🇮🇹 
