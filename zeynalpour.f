59f995a9bf5ae5aed76aaf38c11d3795993b240d
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-08-28 12:42:16 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -2,7 +2,7 @@ ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#23.65G
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#1404/06/13 10:20:21 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#🇳🇱 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@sdk2.myandroidplus2.ir:8080?mode=auto&path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=xhttp&sni=sdk2.myandroidplus2.ir#Tunnel2 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@sdk2.myandroidplus2.ir:8082?path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=grpc&sni=sdk2.myandroidplus2.ir#Tunnel2(grpc) 
+vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@sdk2.myandroidplus2.ir:8082?path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=grpc&sni=sdk2.myandroidplus2.ir#Tunnel2 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:80?path=/&encryption=none&type=xhttp#🇺🇸 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#Tunnel 
 vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
