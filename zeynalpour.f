3d63210cd86ceb6df706eff8141d033db3035601
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-09-04 09:32:01 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,10 +1,10 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#0B/60.00GB (0.0 درصد)
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjUwODEy#زمان اشتراک شما به پایان رسیده
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#🇳🇱 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@sdk2.myandroidplus2.ir:8080?mode=auto&path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=xhttp&sni=sdk2.myandroidplus2.ir#Tunnel2 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@sdk2.myandroidplus2.ir:8082?path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=grpc&sni=sdk2.myandroidplus2.ir#Tunnel2 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:80?path=/&encryption=none&type=xhttp#🇺🇸 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#Tunnel 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@speed.wiselearn.ir:8880?encryption=none&type=grpc#🇫🇮 
-vless://fd1614bb-4fbe-4b7f-bb2f-ba587f4daa3c@finland.vipstore.world:80?mode=auto&path=/api&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇫🇮 
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxwb3VyLUZAbG9jYWxob3N0OjUwODM1#0B/60.00GB (0.0 درصد)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxwb3VyLUZAbG9jYWxob3N0OjUwODM1#1404/07/12 09:20:55 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#🇳🇱 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@sdk2.myandroidplus2.ir:8080?mode=auto&path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=xhttp&sni=sdk2.myandroidplus2.ir#Tunnel2 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@sdk2.myandroidplus2.ir:8082?path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=grpc&sni=sdk2.myandroidplus2.ir#Tunnel2 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@cdn2.myandroidplus2.ir:80?path=/&encryption=none&type=xhttp#🇺🇸 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#Tunnel 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@speed.wiselearn.ir:8880?encryption=none&type=grpc#🇫🇮 
+vless://f4ba9ebf-b0a0-1333-b149-8686d9225e1c@finland.vipstore.world:80?mode=auto&path=/api&security=none&encryption=none&extra=%7B%22scMaxEachPostBytes%22%3A%20%221000000%22%2C%20%22scMaxConcurrentPosts%22%3A%20100%2C%20%22scMinPostsIntervalMs%22%3A%2030%2C%20%22xPaddingBytes%22%3A%20%22100-1000%22%2C%20%22noGRPCHeader%22%3A%20false%7D&type=xhttp#🇫🇮 
