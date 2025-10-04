6d38ac860076edc11e1112dd4febc0e65ff5a943
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-10-05 01:03:11 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,8 +1,6 @@
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#31.18GB/60.00GB (52.0 درصد)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#1404/07/30 01:35:49 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#🇳🇱 
-vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@sdk2.myandroidplus2.ir:8080?mode=auto&path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=xhttp&sni=sdk2.myandroidplus2.ir#Tunnel2 
-vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@sdk2.myandroidplus2.ir:8082?path=/&security=tls&alpn=http/1.1&encryption=none&fp=chrome&type=grpc&sni=sdk2.myandroidplus2.ir#Tunnel2 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:80?path=/&encryption=none&type=xhttp#🇺🇸 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#🇩🇪 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8088?path=/&encryption=none&type=grpc#🇹🇷 
