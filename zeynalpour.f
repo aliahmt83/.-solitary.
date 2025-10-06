9b3a4c7c02b543e9024d3950b5f7a3acfd44dead
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-10-07 00:52:10 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,9 +1,10 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#35.60GB/60.00GB (59.3 درصد)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#35.64GB/60.00GB (59.4 درصد)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#1404/07/30 01:35:49 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:7777?path=/&encryption=none&type=xhttp#🇳🇱 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#🇳🇱 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:80?path=/&encryption=none&type=xhttp#🇺🇸 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#🇩🇪 
+vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8443?security=reality&encryption=none&pbk=Leo19xX145yYV_nhxz4vzwzDlwqjhTQdzRd1aTC6JBo&headerType=none&fp=chrome&type=tcp&sni=store.steampowered.com#انگلیس 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8088?path=/&encryption=none&type=grpc#🇹🇷 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8089?path=/&encryption=none&type=grpc#🇷🇺 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@speed2.wiselearn.ir:80?encryption=none&type=grpc#🇩🇪 
