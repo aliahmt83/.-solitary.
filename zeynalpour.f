dbd43377c70d578ca58c82560dd16b474073c98a
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-10-09 00:02:11 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,4 +1,4 @@
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#39.76GB/60.00GB (66.3 درصد)
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#38.71GB/60.00GB (64.5 درصد)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU3Njc5#1404/07/30 01:35:49 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:7777?path=/&encryption=none&type=xhttp#🇳🇱 
 vless://a683c65b-52bf-3dfe-33bf-aef1f8de63fa@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#🇳🇱 
