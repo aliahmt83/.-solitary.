5dc07a9ec6c649bd8995ed2204d5f0edc71b87d2
Ali Ahmadi <120725468+aliahmt83@users.noreply.github.com>
2025-11-15 19:58:41 +0330

    Updating file content

--- zeynalpour.f
+++ zeynalpour.f
@@ -1,11 +1,11 @@
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU4MTMy#47.61GB/100.00GB (47.6 درصد)
 ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpaZXluYWxQb3VyLUZAbG9jYWxob3N0OjU4MTMy#%D8%A7%D8%B4%D8%AA%D8%B1%D8%A7%DA%A9%20%D8%B4%D9%85%D8%A7%20%D8%AF%D8%B1%2025%20%D8%B3%D8%A7%D8%B9%D8%AA%20%D8%A8%D9%87%20%D9%BE%D8%A7%DB%8C%D8%A7%D9%86%20%D9%85%DB%8C%D8%B1%D8%B3%D8%AF ساعت 
 vless://0958fd48-fd6a-19a4-4315-8df3033189a0@cdn2.myandroidplus2.ir:7777?path=/&encryption=none&type=xhttp#Netherland 🇳🇱 
-vless://0958fd48-fd6a-19a4-4315-8df3033189a0@51.89.14.39:443?security=reality&encryption=none&pbk=Leo19xX145yYV_nhxz4vzwzDlwqjhTQdzRd1aTC6JBo&headerType=none&fp=chrome&type=tcp&sni=fmcloud.fm&flow=xtls-rprx-vision#(این کانفیگ اضطراری دائما تغییر میکند و اشتراک ملزم به بروزرسانی از سوی شما میباشد)🆘 
+vless://0958fd48-fd6a-19a4-4315-8df3033189a0@51.89.14.39:443?security=reality&encryption=none&pbk=Leo19xX145yYV_nhxz4vzwzDlwqjhTQdzRd1aTC6JBo&headerType=none&fp=chrome&type=tcp&sni=fmcloud.fm&flow=xtls-rprx-vision#این سرور مرتبا بروز میشود. لطفا اشتراک خود را بروز کنید 
 vless://0958fd48-fd6a-19a4-4315-8df3033189a0@cdn2.myandroidplus2.ir:8087?path=/&encryption=none&type=grpc#Netherland 🇳🇱 
 vless://0958fd48-fd6a-19a4-4315-8df3033189a0@cdn2.myandroidplus2.ir:80?path=/&encryption=none&type=xhttp#USA 🇺🇸 
 vless://0958fd48-fd6a-19a4-4315-8df3033189a0@cdn2.myandroidplus2.ir:8080?path=/&encryption=none&type=xhttp#Germany 🇩🇪 
-vless://0958fd48-fd6a-19a4-4315-8df3033189a0@puzzle.myandroidplus2.ir:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=tls&fp=chrome&sni=puzzle.myandroidplus2.ir&alpn=http/1.1&allowInsecure=false#اضطراری 2 
+vless://0958fd48-fd6a-19a4-4315-8df3033189a0@puzzle.myandroidplus2.ir:443?encryption=none&flow=xtls-rprx-vision&type=tcp&headerType=none&security=tls&fp=chrome&sni=puzzle.myandroidplus2.ir&alpn=http/1.1&allowInsecure=false#این سرور مرتبا بروز میشود. لطفا اشتراک خود را بروز کنید 
 vless://0958fd48-fd6a-19a4-4315-8df3033189a0@cdn2.myandroidplus2.ir:8443?security=reality&encryption=none&pbk=Leo19xX145yYV_nhxz4vzwzDlwqjhTQdzRd1aTC6JBo&headerType=none&fp=chrome&type=tcp&sni=store.steampowered.com#G.Britain 🇬🇧 
 vless://0958fd48-fd6a-19a4-4315-8df3033189a0@cdn2.myandroidplus2.ir:8088?path=/&encryption=none&type=grpc#Türkiye 🇹🇷 
 vless://0958fd48-fd6a-19a4-4315-8df3033189a0@cdn2.myandroidplus2.ir:8089?path=/&encryption=none&type=grpc#Russia 🇷🇺 
